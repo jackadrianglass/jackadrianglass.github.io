53db225d38f06d7d4619bf7f7ed8e9f373f7fbba
glassjack <jackadrianglass@gmail.com>
2024-11-23 11:41:24 -0700

    chore: Move theme into its own file and document other things

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,7 @@
 module Main exposing (main)
 
+import Theme
+
 import Element exposing (..)
 import Element.Background as Background
 import Element.Font as Font
@@ -11,30 +13,6 @@ import Html.Attributes exposing (style)
 fillerParagraph : String
 fillerParagraph =
     "Lorem ipsum odor amet, consectetuer adipiscing elit. Augue quam cubilia class varius morbi luctus faucibus. Pharetra nisi phasellus duis maximus ex vel sed quis aptent. Sagittis elementum gravida libero aptent dui consequat efficitur per imperdiet. Pharetra auctor mus suspendisse turpis, sem ad ad. Fusce iaculis placerat libero risus ac class potenti integer. Turpis dignissim purus efficitur habitant ad quam placerat. Nascetur nec cubilia hac himenaeos curabitur mi. Bibendum rutrum blandit neque ligula primis dictum dignissim pellentesque!"
-
-
-
--- todo: Get the light version as well https://rosepinetheme.com/palette/ingredients/
-
-
-theme =
-    -- todo: How do I get fonts to be scaled appropriately for headings and content
-    { base = rgb255 25 23 36
-    , surface = rgb255 31 29 46
-    , overlay = rgb255 38 35 58
-    , muted = rgb255 110 106 134
-    , subtle = rgb255 144 140 170
-    , text = rgb255 224 222 244
-    , love = rgb255 235 111 146
-    , gold = rgb255 246 193 119
-    , rose = rgb255 235 188 186
-    , pine = rgb255 49 116 143
-    , foam = rgb255 156 207 216
-    , iris = rgb255 196 167 231
-    , highlightLow = rgb255 33 32 46
-    , highlightMed = rgb255 64 61 82
-    , highlightHigh = rgb255 82 79 103
-    }
 
 
 linkTreeIcon : Icons.Icon -> String -> Element msg
@@ -67,20 +45,20 @@ main =
         -- todo: This is where you put your style options
         -- see what else you can do with this
         -- todo: Add a switch from light mode to dark mode
-        [ Font.color theme.text
+        [ Font.color Theme.theme.text
         , Font.size 18
         , Font.family
             [ Font.typeface "Open Sans"
             , Font.sansSerif
             ]
-        , Background.color theme.base
+        , Background.color Theme.theme.base
         ]
     <|
         column [ width fill, spacing 30 ]
             [ el [ width fill, htmlAttribute <| style "min-height" "100vh", splashScreen ] <|
                 column [ centerX, centerY, padding 5, spacing 5 ]
-                    [ h1 [ centerX, Font.color theme.pine ] "Banana"
-                    , h1 [ centerX, Font.color theme.pine ] "Split"
+                    [ h1 [ centerX, Font.color Theme.theme.pine ] "Banana"
+                    , h1 [ centerX, Font.color Theme.theme.pine ] "Split"
                     , row [ centerX ]
                         [ linkTreeIcon Icons.github "https://github.com/jackadrianglass"
                         , linkTreeIcon Icons.linkedin "https://www.linkedin.com/in/jack-glass-561944129/"
