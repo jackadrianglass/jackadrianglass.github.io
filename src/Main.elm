bf5068675b1dc7304a78e190aa46b8cf84b38aab
glassjack <jackadrianglass@gmail.com>
2024-11-23 12:01:03 -0700

    feat: Port the landing page to a reactive app

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,45 @@
 module Main exposing (main)
 
-import Theme
-import Util
-
+import Browser
 import Element exposing (..)
 import Element.Background as Background
 import Element.Font as Font
 import FeatherIcons as Icons
 import Html exposing (Html)
 import Html.Attributes exposing (style)
+import Theme
+import Util
+
+
+
+-- Main --
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+
+-- Init --
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( {}, Cmd.none )
+
+
+
+-- View --
 
 
 linkTreeIcon : Icons.Icon -> String -> Element msg
@@ -35,8 +66,7 @@ splashScreen =
     behindContent <| el [ height fill, width fill ] <| image [ height fill, centerX ] { src = "https://cdn-icons-png.flaticon.com/512/6482/6482627.png", description = "" }
 
 
-main : Html msg
-main =
+view _ =
     layout
         -- todo: This is where you put your style options
         -- see what else you can do with this
@@ -58,6 +88,7 @@ main =
                     , row [ centerX ]
                         [ linkTreeIcon Icons.github "https://github.com/jackadrianglass"
                         , linkTreeIcon Icons.linkedin "https://www.linkedin.com/in/jack-glass-561944129/"
+
                         -- todo: do something different with the email
                         , linkTreeIcon Icons.mail "jackadrianglass@gmail.com"
                         ]
@@ -65,3 +96,16 @@ main =
             , h1 [ centerX, Font.center ] "more icecream please!"
             , textColumn [ centerX ] <| List.repeat 10 (paragraph [] [ text Util.fillerParagraph ])
             ]
+
+
+
+-- Update --
+
+
+type Msg
+    = Loading
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update _ model =
+    ( model, Cmd.none )
